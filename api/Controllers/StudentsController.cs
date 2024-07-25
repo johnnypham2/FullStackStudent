cf16e38cf35bbe8d8859bc867368d37646617315
Johnnypham2 <phammjohnny@gmail.com>
2024-07-24 21:42:28 -0700

    first commit

---
+++ api/Controllers/StudentsController.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+       public StudentsController(AppDbContext context)
+       {
+        _context = context;
+       }
+       
+       [HttpGet]
+
+        
+        public async Task<IEnumerable<Student>> GetStudents()
+        {
+            var students = await _context.Students.AsNoTracking().ToListAsync();
+            return students;
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Create(Student student)   
+
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            await _context.AddAsync(student);
+            
+            var result = await _context.SaveChangesAsync();
+
+            if(result > 0)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+    
+    [HttpDelete("{id:int}")]
+
+    public async Task<IActionResult> Delete(int id)
+    {
+       var student = await _context.Students.FindAsync(id);
+       if(student == null) 
+       {
+        return NotFound();
+       }
+
+       _context.Remove(student);
+
+       var result = await _context.SaveChangesAsync();
+
+        if(result > 0)
+        {
+            return Ok("Student was deleted");
+        }
+        return BadRequest("Unable to delete student");
+
+    }
+
+    //single student
+
+    [HttpGet("{id:int}")]
+
+    public async Task<ActionResult<Student>> GetStudents(int id)
+    {
+        var student = await _context.Students.FindAsync(id);
+        if (student == null)
+        {
+            return NotFound("who dis");
+        }
+        return Ok(student);
+    }
+
+
+//update/put
+    [HttpPut("{id:int}")]
+
+    public async Task<IActionResult> EditStudent(int id, Student student)
+    {
+        var studentFromDb = await _context.Students.FindAsync(id);
+         if (studentFromDb == null)
+         {
+            return BadRequest("Student not found");
+         }
+         studentFromDb.Name = student.Name;
+         studentFromDb.Email = student.Email;
+         studentFromDb.PhoneNumber = student.PhoneNumber;
+         studentFromDb.Address = student.Address;
+
+         var result = await _context.SaveChangesAsync();
+
+         if(result > 0)
+         {
+            return Ok("Student was edited");
+         }
+         return BadRequest("Unable to update data");
+    }
+    
+    }
+}
---
+++ api/Program.cs
@@ -0,0 +1,29 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.Run();
+
